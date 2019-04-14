f54434017a6dc73775986d7d7f031e3a81e4019a
Xu Huang <xht8777@joker.ece.northwestern.edu>
2019-04-13 21:51:20 -0500

    Need Check

--- ps.c
+++ ps.c
@@ -8,16 +8,9 @@ int main(int argc, char *argv[])
     int count = getprocs(Table);
     int i;
     for (i = 0; i < count; i++){
-        char* State;
-        switch(Table[i].state){
-            case 0: State = "UNUSED";
-            case 1: State = "EMBRYO";
-            case 2: State = "SLEEPING";
-            case 3: State = "RUNNABLE";
-            case 4: State = "RUNNING";                    
-            case 5: State = "ZOMBIE";
-        }
-        printf(0,"%d %s %d %s\n", Table[i].ppid, State, Table[i].pid, Table[i].name);
+        char* State [] = {"UNUSED", "EMBRYO", "SLEEPING", "RUNNABLE", "RUNNING", "ZOMBIE" };
+
+        printf(0,"%d  %d  %s  %d  %s\n",Table[i].pid, Table[i].ppid, State[Table[i].state], Table[i].sz, Table[i].name);
     }
 
     exit();
